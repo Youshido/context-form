956a77a141d393e1d2a66333af203d73d4eeb7bc
Alexandr Viniychuk <a@viniychuk.com>
2018-07-06 09:57:18 -0400

    removed css

--- src/Theme/SimpleTheme/SimpleTheme.js
+++ src/Theme/SimpleTheme/SimpleTheme.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import './SimpleTheme.css';
+// import './SimpleTheme.css';
 
 const Input = (props) => {
   const { required, ...extra } = props;
